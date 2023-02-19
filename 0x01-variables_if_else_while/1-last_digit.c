51efabe4b19163c20fee7b8b67566724dd2c31be
computerwizard1234 <mgasa.loucat1@gmail.com>
2023-02-18 21:00:10 -0800

    My first commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.>
+
+int main(void)
+{
+        int n;
+
+        srand(time(0);
+                        n = rand() - RAND_MAX / 2;
+			l = n % 10;
+
+                        if (l > 5)
+                        {
+                        printf("Last digit of %d is % and is greater that 5", n, l)
+                        }
+                        else if (l== 0 )
+                        {
+                        printf("Last digitof %d is %d and is 0\n", n, l)
+                        }
+                        else if (n==0)
+                                print("Last digit of %d is %d and is less than 6 and not 0", n, l);
+
+                        return (0);
+
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+        char alp[26] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+
+        for (i=0; i < 52; i++)
+        {
+                putchar(alp[i]);
+        }
+
+        putchar('\n');
+
+        return (0);
+
+}
