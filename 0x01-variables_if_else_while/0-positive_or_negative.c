1440c5edeb3c6b3ecfc269ef091b24823c601700
computerwizard1234 <mgasa.loucat1@gmail.com>
2023-02-20 11:37:02 -0800

    My first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ int main(void)
 			{
 			printf("%d is positive\n", n)
 			}
-			else if (n<0)
+			else if (n < 0)
 			{
 			printf("%d is negative\n", n)
 			}
-			else if (n==0)
+			else if (n == 0)
 				print("%d is zero\n", n);
 
 			return (0);
