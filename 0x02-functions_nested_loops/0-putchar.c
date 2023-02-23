e5cf5ca80e29e82f262ad3107a607e936d0761e3
computerwizard1234 <mgasa.loucat1@gmail.com>
2023-02-23 08:01:12 -0800

    My another commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main-print _putchar and a new line
- *
- *Return: Always 0
-*/
+ * main-prints _putchar and a new line
+ * Return: Always 0
+ */
 int main(void)
 {
 	_putchar('_');
