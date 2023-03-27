65f2316d0f65f9cc81181029d79f109253a17fd4
computerwizard1234 <mgasa.loucat1@gmail.com>
2023-03-26 23:37:49 -0700

    Done with the task correction

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,6 @@
 /**
  * sum_them_all - returns the sum of all its parameters.
  * @n: amount of the arguments.
- *
  * Return: sum of its parameters.
  */
 int sum_them_all(const unsigned int n, ...)
