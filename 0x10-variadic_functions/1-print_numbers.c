7c3047fcc2b070f3eaa51e74a5034caa4f5e51cf
computerwizard1234 <mgasa.loucat1@gmail.com>
2023-03-26 21:22:57 -0700

    Done with the task correction

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,27 +1,26 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - print numbers.
+ * print_numbers - prints numbers.
  * @separator: string to be printed between numbers.
- * @n: number of intergers passed to the function
+ * @n: number of integers passed to the function.
  *
- * Return: no return
+ * Return: no return.
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list valist;
-unsigned int i;
+	va_list valist;
+	unsigned int i;
 
-va_start(valist, n);
+	va_start(valist, n);
 
-for (i = 0; i < n; i++)
-{
-print("%d", va_arg(valist, int));
-if (separator && i < n - 1)
-printf("%s", separator);
-}
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
 
-printf("\n");
-va_end(valist);
+	printf("\n");
+	va_end(valist);
 }
